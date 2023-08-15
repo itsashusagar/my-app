ac66fd60cf034a8864a75d98235f0dd67c979114
ashu sagar <itsashusagar@gmail.com>
2023-08-15 23:18:58 +0530

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,40 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
 
-export default function Home() {
+import React, { useState, useEffect } from "react";
+
+export default async function page() {
+  // const [data, setData] = useState([]);
+
+  const response = await fetch("https://dummyjson.com/products");
+  const jsonData = await response.json();
+
+  // useEffect(() => {
+
+  //   fetchData();
+  // }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div>
+      <h2 style={{textAlign:"center" , fontSize:"5rem" , color:"pink"}}>Data Fetching Example</h2>
+      <ul>
+        {jsonData.products.map((item) => (
+          <>
+            <div key={item.title}   style={{border:"1px solid pink", padding:"50px", margin: "20px"}}>
+              <h1 style={{ textAlign: "center" , paddingBottom:'18px' , fontWeight: "900" , fontSize: "3rem"}}>{item.title}</h1>
+              <img
+                key={item.id}
+                src={item.thumbnail}
+                // width={500}
+                style={{ paddingLeft: "33%" }}
+                alt={item.title}
+              />
+              <h1 style={{textAlign: "center" , paddingTop:'18px'}}>{item.description}</h1>
+            </div>
+          </>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+//   <li key={item.id}>{item.body}</li>
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "../components/header.css"
+
+
+const Header = () => {
+  return (
+    <>
+    <div>
+
+    <a href="">Ashu Portfolio</a>
+     <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Contact Us</a></li>
+        <li><a href="">Projects</a></li>
+        <li><a href="">Privacy</a></li>
+        <li><a href="">Languages</a></li>
+     </ul>
+    </div>
+
+
+
+   {/* img src="https://png.pngtree.com/png-clipart/20190115/ourmid/pngtree-wenjing-girl-cute-girl-q-version-girl-cute-little-girl-png-image_336974.jpg" alt="" /> */}
+</>
+  )
+}
+
+export default Header
